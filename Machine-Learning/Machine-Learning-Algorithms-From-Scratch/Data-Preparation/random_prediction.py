f26dba27b5a865594d00695737825abfcdbcf1bb
Dongchan <dongchanyear@gmail.com>
2022-07-12 09:55:15 +0800

    random prediction example

---
+++ Machine-Learning/Machine-Learning-Algorithms-From-Scratch/Data-Preparation/random_prediction.py
@@ -0,0 +1,11 @@
+from random import seed
+
+def random_algorithm(train, test):
+    predicted = list()
+    return predicted
+
+seed(1) 
+train = [[0], [1], [0], [1], [0], [1]]
+test = [[None], [None], [None], [None]]
+predictions = random_algorithm(train, test)
+print(predictions)
